168d07e15ddff260c835f4c99b6ee316b9e334d2
Rahulkesharwani353 <rahul.kesharwani@cm.com>
2024-02-26 15:42:55 +0530

    Get ALL Task and mark completed or not

--- src/app/components/pages/all-task/all-task.component.ts
+++ src/app/components/pages/all-task/all-task.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpService } from '../../../services/http.service';
+import { TaskListComponent } from '../task-list/task-list.component';
+import { StateService } from '../../../services/state.service';
 
 @Component({
   selector: 'app-all-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, TaskListComponent],
   templateUrl: './all-task.component.html',
   styleUrl: './all-task.component.scss'
 })
-export class AllTaskComponent {
 
+export class AllTaskComponent {
+  newTask = '';
+  intialTaskList: any[] = [];
+  taskList: any[] = [];
+  httpService = inject(HttpService);
+  stateService = inject(StateService);
+
+  ngOnInit() {
+    this.stateService.searchSubject.subscribe((value) => {
+      console.log("search",value)
+      if (value) {
+        this.taskList = this.intialTaskList.filter((x) =>
+          x.title.toLowerCase().includes(value.toLowerCase())
+        );
+      }else{
+        this.taskList=this.intialTaskList;
+      }
+    });
+    this.getAllTasks();
+  }
+
+  addTask() {
+    console.log('addTask', this.newTask);
+    this.httpService.addTask(this.newTask).subscribe(() => {
+      this.newTask = '';
+      this.getAllTasks();
+    });
+  }
+
+  getAllTasks() {
+    this.httpService.getAllTasks().subscribe((result: any) => {
+      this.intialTaskList = this.taskList = result;
+    });
+  }
+
+  onComplete(task: any) {
+    task.completed = !task.completed;
+    console.log('complete', task);
+    this.httpService.updateTask(task).subscribe(() => {
+      this.getAllTasks();
+    });
+  }
+
+  onImportant(task: any) {
+    task.important = !task.important;
+    this.httpService.updateTask(task).subscribe(() => {
+      this.getAllTasks();
+    });
+  }
 }
